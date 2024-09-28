ccef64bd00ed4307e852eb97519706d12777ba00
Dylane <tanolamene@gmail.com>
2024-09-28 14:01:35 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="quiz">
+    <div class="quiz__title">Questionnaire sur les Films et series</div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
